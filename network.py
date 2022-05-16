7f57e48f121dec1d2e8f32e34ff111b1c0613d4f
poottipong rueangsuntikornkul <6210520117@student.tu.ac.th>
2022-05-16 16:25:20 +0700

    start connecting

--- network.py
+++ network.py
@@ -4,3 +4,14 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.IF_INET, socket.SCOK_STEAM)
+        self.server = "192.168.253.114"
+        self.port = "8000"
+        self.addr = (self.server, self.port)
+        self.connect()
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass

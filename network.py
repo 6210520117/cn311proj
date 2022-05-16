deb34099a1b2dcb6d6296a9ea26c6873540f1b20
poottipong rueangsuntikornkul <6210520117@student.tu.ac.th>
2022-05-16 16:50:17 +0700

    connect to server done

--- network.py
+++ network.py
@@ -3,11 +3,12 @@ import socket
 
 class Network:
     def __init__(self):
-        self.client = socket.socket(socket.IF_INET, socket.SCOK_STEAM)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "192.168.253.114"
-        self.port = "8000"
+        self.port = 8000
         self.addr = (self.server, self.port)
-        self.connect()
+        self.id = self.connect()
+        print(self.id)
 
     def connect(self):
         try:
@@ -15,3 +16,6 @@ class Network:
             return self.client.recv(2048).decode()
         except:
             pass
+
+
+n = Network()

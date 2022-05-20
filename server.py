91d85cd72828af1622a95dabd7f3fc3435a2941a
poottipong rueangsuntikornkul <6210520117@student.tu.ac.th>
2022-05-20 17:13:40 +0700

    Update game

--- server.py
+++ server.py
@@ -1,58 +1,63 @@
 import pickle
 import socket
 from _thread import *
-from player import *
-import sys
+from game import Game
 
-server = "192.168.253.114"
+server = "192.168.1.5"
 port = 8000
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    s.bind((server, port))
-except socket.error as e:
-    str(e)
-
-s.listen(2)
-print("Waiting for a connection, Server Started")
-
-players = [Player(0, 0, 50, 50, (255, 0, 0)),
-           Player(100, 100, 50, 50, (0, 0, 255))]
+s.bind((server, port))
+s.listen()
+print("Waiting...")
+games = {}  # store game
+idCount = 0
 
 
-def threaded_client(conn, player):
-    conn.send(pickle.dumps(players[player]))
-    reply = ""
+def threaded_client(conn, p, gameId):
+    global idCount
+    conn.send(str.encode(str(p)))
+
     while True:
         try:
-            data = pickle.loads(conn.recv(2048))  # amount recv information
-            players[player] = data
+            data = conn.recv(4096).decode()
 
-            if not data:
-                print("Disconnected")
-                break
-            else:
-                if player == 1:
-                    reply = players[0]
+            if gameId in games:
+                game = games[gameId]  # check game ว่ายังอยู่ในlist หรือป่าว
+
+                if not data:
+                    break
                 else:
-                    reply = players[1]
+                    if data == "reset":
+                        game.resetWent()
+                    elif data != "get":
+                        game.play(p, data)
 
-                print("Received: ", data)
-                print("Sending: ", reply)
-
-            conn.sendall(pickle.dumps(reply))
+                    conn.sendall(pickle.dumps(game))
+            else:
+                break
         except:
             break
 
-    print("Lost connection")
+    try:
+        del games[gameId]
+        print(f'Game:{gameId} Player left the game')
+    except:
+        pass
+    idCount -= 1
     conn.close()
 
 
-playerNumber = 0
-# wait for connection 2
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
-    start_new_thread(threaded_client, (conn, playerNumber))
-    playerNumber += 1
+    idCount += 1
+    p = 0
+    gameId = (idCount - 1) // 2  # มอบสิ่งระบุตัวตน 0 vs 0 1 vs 1
+    if idCount % 2 == 1:
+        games[gameId] = Game(gameId)
+        print("Find a suitable opponent...")
+    else:
+        games[gameId].ready = True
+        p = 1
+
+    start_new_thread(threaded_client, (conn, p, gameId))
